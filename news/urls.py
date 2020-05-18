66a1f4bc3f222d73f3cb4e9a3e309ea507b86a19
JeromeMberia <jeromemberia@gmail.com>
2020-05-19 00:53:08 +0300

    Adding Dynamic URL

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('today/', views.news_of_day, name='newsToday'),
-    
+    path('archives/<int:year>/<int:month>/<int:day>/', views.past_days_news,name = 'pastNews'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 import datetime as dt
 
 
@@ -31,4 +31,26 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+def past_days_news(request,past_date):
+    # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
