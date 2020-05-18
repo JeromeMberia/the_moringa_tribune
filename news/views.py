af8ec4fc5837e86092aa66fac2487202406419a6
JeromeMberia <jeromemberia@gmail.com>
2020-05-19 01:00:23 +0300

    Template Inheritance

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import Http404, HttpResponse
 import datetime as dt
 
@@ -8,17 +8,7 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 def convert_dates(dates):
     
@@ -31,19 +21,22 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_news(request,past_date):
-    # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+# View Function to present news from past days
+def past_days_news(request, past_date):
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
 
 def past_days_news(request,past_date):
     
